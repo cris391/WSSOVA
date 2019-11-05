bfa516a74a6d45d22f7659c6f2cb530e0646d731
guest1 <guest1@guest1s-MacBook-Pro.local>
2019-11-05 13:50:04 +0100

    added question controller, mapping of question, model

---
+++ DatabaseService/SOContext.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using DatabaseService;
+
+
+namespace DatabaseService
+{
+  public class SOContext : DbContext
+  {
+    public DbSet<Question> Questions { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+      optionsBuilder.UseNpgsql(connectionString: "host=localhost;db=stackoverflow;uid=postgres;pwd=root");
+    }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+      modelBuilder.Entity<Question>().ToTable("questions");
+      modelBuilder.Entity<Question>().Property(m => m.Id).HasColumnName("questionid");
+      modelBuilder.Entity<Question>().Property(m => m.ClosedDate).HasColumnName("closeddate");
+      modelBuilder.Entity<Question>().Property(m => m.Title).HasColumnName("title");
+
+      // modelBuilder.Entity<OrderDetails>().ToTable("orderdetails");
+      // modelBuilder.Entity<OrderDetails>().Property(m => m.OrderId).HasColumnName("orderid");
+      // modelBuilder.Entity<OrderDetails>().Property(m => m.ProductId).HasColumnName("productid");
+      // modelBuilder.Entity<OrderDetails>().Property(m => m.UnitPrice).HasColumnName("unitprice");
+      // modelBuilder.Entity<OrderDetails>().Property(m => m.Quantity).HasColumnName("quantity");
+      // modelBuilder.Entity<OrderDetails>().Property(m => m.Discount).HasColumnName("discount");
+      // modelBuilder.Entity<OrderDetails>().HasKey(m => new { m.OrderId, m.ProductId });
+    }
+  }
+}
---
+++ WebApi/Controllers/QuestionsController.cs
@@ -0,0 +1,70 @@
+﻿using DatabaseService;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApi.Controllers
+{
+  [ApiController]
+  [Route("api/questions")]
+  public class QuestionsController : ControllerBase
+  {
+    IDataService _dataService;
+    public QuestionsController(IDataService dataService)
+    {
+      _dataService = dataService;
+    }
+
+      [HttpGet]
+      public IList<Question> GetQuestions()
+      {
+        return _dataService.GetQuestions();
+      }
+
+      [HttpGet("{questionId}")]
+      public ActionResult<Question> GetQuestion(int questionId)
+      {
+        var question = _dataService.GetQuestion(questionId);
+
+        if (question == null) return NotFound();
+
+        return Ok(question);
+      }
+
+    //   [HttpPost]
+    //   public ActionResult CreateCategory([FromBody] Category category)
+    //   {
+    //     var cat = _dataService.CreateCategory(category.Name, category.Description);
+
+    //     return Created("post", cat);
+    //   }
+
+    //   [HttpPut("{categoryId}")]
+    //   public ActionResult PutCategory([FromBody] Category category, int categoryId)
+    //   {
+    //     var cat = _dataService.PutCategory(categoryId, category.Name, category.Description);
+    //     if (cat == false)
+    //     {
+    //       return NotFound();
+    //     }
+
+    //     return Ok(cat);
+    //   }
+
+    //   [HttpDelete("{categoryId}")]
+    //   public ActionResult<Category> DeleteCategory(int categoryId)
+    //   {
+    //     // var category = _dataService.DeleteCategory(categoryId);
+
+    //     if (_dataService.DeleteCategory(categoryId) == false)
+    //     {
+    //       return NotFound();
+    //     }
+
+    //     return Ok();
+    //   }
+  }
+}
