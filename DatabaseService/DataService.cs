45c2c3fd82d6386615fab47dfb989705680e52db
cris391 <cirstianpand@yahoo.com>
2019-11-14 10:29:43 +0100

    log exceptions

--- DatabaseService/DataService.cs
+++ DatabaseService/DataService.cs
@@ -140,7 +140,7 @@ namespace DatabaseService
       }
       catch (System.Exception e)
       {
-
+        Console.WriteLine(e);
         return 0;
       }
     }
@@ -193,6 +193,7 @@ namespace DatabaseService
       }
       catch (System.Exception e)
       {
+        Console.WriteLine(e);
         return false;
       }
     }
@@ -218,6 +219,7 @@ namespace DatabaseService
       }
       catch (System.Exception e)
       {
+        Console.WriteLine(e);
         return false;
       }
     }
@@ -232,131 +234,9 @@ namespace DatabaseService
       }
       catch (System.Exception e)
       {
+        Console.WriteLine(e);
         return false;
       }
     }
-    // public Category CreateCategory(string name, string description)
-    // {
-    //   using var db = new NorthwindContext();
-    //   var nextId = db.Categories.Max(x => x.Id) + 1;
-    //   var category = new Category { Id = nextId, Name = name, Description = description };
-    //   db.Categories.Add(category);
-    //   db.SaveChanges();
-
-    //   return GetCategory(category.Id);
-    // }
-
-
-    // public bool DeleteCategory(int id)
-    // {
-    //   using var db = new NorthwindContext();
-    //   var category = GetCategory(id);
-    //   try
-    //   {
-    //     db.Categories.Remove(category);
-    //     db.SaveChanges();
-    //   }
-    //   catch (System.Exception e)
-    //   {
-    //     return false;
-    //   }
-    //   return true;
-    // }
-
-    // public bool UpdateCategory(int id, string name, string description)
-    // {
-    //   try
-    //   {
-    //     using var db = new NorthwindContext();
-    //     var category = GetCategory(id);
-    //     category.Name = name;
-    //     category.Description = description;
-    //     db.Update(category);
-    //     db.SaveChanges();
-    //   }
-    //   catch (System.Exception e)
-    //   {
-    //     return false;
-    //   }
-    //   return true;
-    // }
-    // public bool PutCategory(int id, string name, string description)
-    // {
-    //   try
-    //   {
-    //     using var db = new NorthwindContext();
-    //     var category = GetCategory(id);
-    //     category.Name = name;
-    //     category.Description = description;
-    //     db.Update(category); 
-    //     db.SaveChanges();
-    //   }
-    //   catch (System.Exception e)
-    //   {
-    //     return false;
-    //   }
-    //   return true;
-    // }
-
-    // public Product GetProduct(int id)
-    // {
-    //   using var db = new NorthwindContext();
-    //   return db.Products
-    //     .Where(p => p.Id == id)
-    //     .Include(p => p.Category)
-    //     .FirstOrDefault();
-    // }
-
-    // public List<Product> GetProductByCategory(int id)
-    // {
-    //   using var db = new NorthwindContext();
-    //   return db.Products
-    //     .Where(p => p.CategoryId == id)
-    //     .Include(p => p.Category)
-    //     .ToList();
-    // }
-
-    // public List<Product> GetProductByName(string name)
-    // {
-    //   using var db = new NorthwindContext();
-    //   return db.Products
-    //     .Where(p => p.Name.Contains(name))
-    //     .ToList();
-    // }
-
-    // public Order GetOrder(int id)
-    // {
-    //   using var db = new NorthwindContext();
-    //   return db.Orders
-    //     .Where(p => p.Id == id)
-    //     .Include(p => p.OrderDetails)
-    //     .ThenInclude(p => p.Product)
-    //     .ThenInclude(p => p.Category)
-    //     .FirstOrDefault();
-    // }
-
-    // public List<Order> GetOrders()
-    // {
-    //   using var db = new NorthwindContext();
-    //   return db.Orders.ToList();
-    // }
-
-    // public List<OrderDetails> GetOrderDetailsByOrderId(int id)
-    // {
-    //   using var db = new NorthwindContext();
-    //   return db.OrderDetails
-    //   .Where(m => m.OrderId == id)
-    //   .Include(m => m.Product)
-    //   .ToList();
-    // }
-
-    // public List<OrderDetails> GetOrderDetailsByProductId(int id)
-    // {
-    //   using var db = new NorthwindContext();
-    //   return db.OrderDetails
-    //   .Where(m => m.ProductId == id)
-    //   .Include(m => m.Order)
-    //   .ToList();
-    // }
   }
 }
