059a1903b3bc96eac1080ac5d461e1f2f014ba2e
cris391 <cirstianpand@yahoo.com>
2019-11-19 08:27:29 +0100

    fix Owner CreationDate

--- WebApi/MOdels/OwnerDto.cs
+++ WebApi/MOdels/OwnerDto.cs
@@ -5,7 +5,7 @@ namespace WebApi.Models
   public class OwnerDto
   {
     public string Link { get; set; }
-    public DateTime CreationDate { get; set; }
+    public DateTime UserCreationDate { get; set; }
     public string UserDisplayName { get; set; }
     public string UserLocation { get; set; }
     public int UserAge { get; set; }
