c71daff7f7c49aab40b45d6b588bf4fc319f9f14
cris391 <cirstianpand@yahoo.com>
2019-11-21 10:04:46 +0100

    added a warmer service

--- WebApi/Startup.cs
+++ WebApi/Startup.cs
@@ -11,7 +11,8 @@ using Microsoft.Extensions.Hosting;
 using WebApi.Middleware;
 using System.Text;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.IdentityModel.Tokens; 
+using Microsoft.IdentityModel.Tokens;
+using WebApi.Controllers;
 
 namespace WebApi
 {
@@ -28,26 +29,26 @@ namespace WebApi
     public void ConfigureServices(IServiceCollection services)
     {
       services.AddControllers();
-      
+
       services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
       services.AddSingleton<IDataService, DataService>();
 
-            var key = Encoding.UTF8.GetBytes(Configuration.GetSection("Auth:Key").Value);
+      var key = Encoding.UTF8.GetBytes(Configuration.GetSection("Auth:Key").Value);
 
-            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
-            {
-                options.TokenValidationParameters = new TokenValidationParameters
-                {
-                    ValidateIssuerSigningKey = true,
-                    IssuerSigningKey = new SymmetricSecurityKey(key),
-                    ValidateIssuer = false,
-                    ValidateLifetime = true,
-                    ValidateAudience = false,
-                    ClockSkew = TimeSpan.Zero
-                };
-            });
-
-        }
+      services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+      {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+          ValidateIssuerSigningKey = true,
+          IssuerSigningKey = new SymmetricSecurityKey(key),
+          ValidateIssuer = false,
+          ValidateLifetime = true,
+          ValidateAudience = false,
+          ClockSkew = TimeSpan.Zero
+        };
+      });
+      services.AddSingleton<IApiWarmer, ApiWarmer>();
+    }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
     public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
