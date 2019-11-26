83d2b165cd0054c4fc08afd68a91ee887ebc63d6
cris391 <cirstianpand@yahoo.com>
2019-11-26 11:43:12 +0100

    delete  uneeded modules

--- WebApi/Warmup/IApiWarmer.cs
+++
@@ -1,9 +0,0 @@
-using System;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-
-public interface IApiWarmer
-{
-  Task WarmUp<TStartup>(IServiceProvider serviceProvider, IUrlHelper urlHelper, HttpRequest request, params string[] ignoredControllers);
-}
